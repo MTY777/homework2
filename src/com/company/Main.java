1b9a23b86399260bc5d3920a977ddf8f462fd190
MTY777 <mataymekkurmanbek@gmail.com>
2021-12-05 00:41:12 +0600

    Home work

---
+++ src/com/company/Main.java
@@ -0,0 +1,42 @@
+package com.company;
+
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(rec(30, 24));
+        System.out.println(rec(-45, 32));
+        System.out.println(rec(12, 89));
+        System.out.println(rec(13, 56));
+        System.out.println(rec(-12, 84));
+
+        System.out.println(generateRandomAge() + rec(generateRandomAge(),10));
+    }
+
+    public static String rec(int tempereture, int age) {
+        String toy = "Можно идти гулять";
+        String toy1 = "Нельзя идти гулять";
+        if (age >= 20 && age <= 45 && tempereture >= -20 && tempereture <= 30) {
+            return toy;
+        } else if (age <= 20 && tempereture > 0 && tempereture <= 28) {
+            return toy;
+        }
+
+        else
+        {
+            return toy1;
+        }
+
+
+    }
+    public static int generateRandomAge(){
+        Random random = new Random();
+        int a = random.nextInt(10000);
+        return a;
+    }
+}
+
+
+
+
